6896c6e1d1d785c0a77dea303285491dfb7b2cd6
Peppesergee <giuseppesergi13@gmail.com>
2024-10-07 09:46:16 +0200

    Update test_api.py

--- application_code/test_api.py
+++ application_code/test_api.py
@@ -7,23 +7,35 @@ from transformers import (
     BitsAndBytesConfig,
 )
 
-base_model = "swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA"
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    # bnb_4bit_quant_type="nf4",
-    # bnb_4bit_compute_dtype=torch.bfloat16,
-    # bnb_4bit_use_double_quant=False,
-)
-model = AutoModelForCausalLM.from_pretrained(
-    base_model,
-    quantization_config=bnb_config,
-    device_map="auto",  # "auto" usa la GPU se disponibile
-)
-tokenizer = AutoTokenizer.from_pretrained(base_model)
+# Variabili globali per tenere traccia dello stato del modello e tokenizer
+model = None
+tokenizer = None
+
+def initialize_model():
+    global model, tokenizer
+
+    if model is None or tokenizer is None:
+        print("Caricamento del modello...")
+        base_model = "swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA"
+        bnb_config = BitsAndBytesConfig(
+            load_in_8bit=True,
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model,
+            quantization_config=bnb_config,
+            device_map="auto",  # "auto" usa la GPU se disponibile
+        )
+        tokenizer = AutoTokenizer.from_pretrained(base_model)
+        print("Modello caricato correttamente.")
+    else:
+        print("Modello già caricato, utilizzo quello esistente.")
 
 sys = "Sei un assistente AI per la lingua Italiana di nome LLaMAntino-3 ANITA " \
       "(Advanced Natural-based interaction for the ITAlian language)." \
       " Rispondi nella lingua usata per la domanda in modo chiaro, semplice ed esaustivo."
+
+# Inizializza il modello solo se necessario
+initialize_model()
 
 pipe = transformers.pipeline(
     model=model,
@@ -37,9 +49,10 @@ pipe = transformers.pipeline(
 )
 
 while True:
+    # Richiede la domanda all'utente e aspetta che premi invio
     user_question = input("Inserisci la tua domanda (o digita 'esci' per uscire): ")
 
-    # uscita
+    # uscita dal ciclo
     if user_question.lower() == 'esci':
         print("Uscita dal programma.")
         break
